c1a53d40a572efd00c1da2c14d21a4262f507d19
oxaoz <ogozer@ya.ru>
2022-10-26 01:10:28 +0700

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = generateRandomArray();
+        task1();
     }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
+        int sum = 0;
+        int[] arr = generateRandomArray();
+        for (int numbers : arr) {
+            sum += numbers;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+    }
+
+
+
 }
+
+
+
+
+
